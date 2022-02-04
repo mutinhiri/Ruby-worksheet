e1f3ea48a22289ec3dc7490fc1bbe938e4095450
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 19:36:19 +0200

    Add array loop for strings

--- printing.rb
+++ printing.rb
@@ -4,3 +4,6 @@ puts formatter % [1,2,3,4]
 puts formatter % ['one' , 'two', 'three', 'four']
 puts formatter % [true, false, false, true]
 puts formatter % [formatter, formatter, formatter, formatter]
+puts formatter % [
+  "I had this thing"
+]
