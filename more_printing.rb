70135feaa11983c3a838f9b2527dde6921211711
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 19:08:24 +0200

    Add more implementation of the app

--- more_printing.rb
+++ more_printing.rb
@@ -2,3 +2,6 @@ puts "Mary had a little lamb"
 puts "its fleece was as white as %s." %'snow'
 puts "and everywhere that Mary went"
 puts "." * 10  #check the changes it ouptut to the console
+
+end1 = 'C'
+end2 = 'H'
