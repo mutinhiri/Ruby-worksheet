5767fd03c681ec7c37dc7b3a6aa53f9e5a4db91f
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 13:21:16 +0200

    Add changes for personality

--- more_variables.rb
+++ more_variables.rb
@@ -10,4 +10,4 @@ puts "Lets talk about %s." % my_name
 puts "He's %s metres tall," %my_height
 puts "He's %d Kg heavy" %my_weight
 puts "Isn't that too heavy noo at least"
-puts "he's got %s eyes and %s hair" %[my_eyes, my_age]
+puts "he's got %s eyes and %s hair" %[my_eyes, my_hair]
