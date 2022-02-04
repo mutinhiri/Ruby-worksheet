758b04dd0e459a024c549d0d4e8df2bf9ebd345e
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 19:07:12 +0200

    Test the app if working properly

--- more_printing.rb
+++ more_printing.rb
@@ -1,4 +1,4 @@
 puts "Mary had a little lamb"
 puts "its fleece was as white as %s." %'snow'
 puts "and everywhere that Mary went"
-puts "." * 10 
+puts "." * 10  #check the changes it ouptut to the console
