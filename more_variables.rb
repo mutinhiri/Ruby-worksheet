87d042b64d04b9c4f79e856bada9f72dde7a5157
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 13:46:59 +0200

    Fix bug for summing

--- more_variables.rb
+++ more_variables.rb
@@ -12,4 +12,4 @@ puts "He's %d Kg heavy" %my_weight
 puts "Isn't that too heavy noo at least"
 puts "he's got %s eyes and %s hair" %[my_eyes, my_hair]
 puts "His teeth are usua;lly %s depending on %d " %[my_teeth, my_age]
-puts "If you add %d, %d, %d i get %d " %[my_age, my_height, my_weight, my_age + my_age+ my_height + my_height ]
+puts "If you add %d, %d, %d i get %d " %[my_age, my_height, my_weight,  my_age+ my_height + my_height ]
