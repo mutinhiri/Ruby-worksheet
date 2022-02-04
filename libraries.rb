468eafa6a37e27e3fa077847ff690c840207b6fb
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-05 01:15:56 +0200

    Add changes and fix error

--- libraries.rb
+++ libraries.rb
@@ -1,9 +1,10 @@
 require 'open-url'
 
-open(http://www.google.com) do |f|
+open("http://www.google.com") do |f|
   f.each_line {|line| p line}
   puts f.base_url
   puts f.content_type
   puts f.charset
   puts f.consent_encoding
   puts f.last_modified
+end
