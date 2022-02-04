dfaafa8dc6d4322a0ee4f562eb671eee80391bc9
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 18:49:48 +0200

    Add changes for naming fix

--- strings.rb
+++ strings.rb
@@ -3,3 +3,6 @@ name = 'Barnabas Bunbee'
 name2 = 'Great'
 
 puts "The name of the man is #{name } and the name of the second partner is #{name2}"
+
+x = "there are #{10} types of people"
+binary = 'binary'
