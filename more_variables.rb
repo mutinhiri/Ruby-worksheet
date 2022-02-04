f5cf79bcb99c5805a9177f5ded4fd924f404c6c8
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-04 13:16:54 +0200

    Add checks

--- more_variables.rb
+++ more_variables.rb
@@ -9,3 +9,4 @@ my_hair = 'Brownish' #not sure if color is available
 puts "Lets talk about %s." % my_name
 puts "He's %s metres tall," %my_height
 puts "He's %d Kg heavy" %my_weight
+puts "Isn't that too heavy noo at least"
