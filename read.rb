4895b55d30cc3d3d419b5ec7b361cba1394b42b6
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-06 20:31:13 +0200

    Change script to write to file

--- read.rb
+++ read.rb
@@ -1,7 +1,7 @@
 filename = ARGV.first
 script = $0
 
-puts "we're going to erase #{filename}."
+puts "we're going to erase #{script}."
 puts "I'f you dont want that , hit CTRL-C "
 puts "If you do want that hit RETURn"
 
