16003c70fa5cad2e40aa0f90c3afa93a1c452115
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-06 20:16:44 +0200

    Store user line 1 input

--- read.rb
+++ read.rb
@@ -14,4 +14,4 @@ target = File.open(filename, 'w')
 puts "Truncating the file. Goodbye!"
 target.truncate(target.size)
 puts "Now I'm going to ask you for 3 lines"
-print "line 1: "; line1 =
+print "line 1: "; line1 = STDIN.gets.chomp()
