9584a61b05497eb1b1567a4c76a9494c2f58ac73
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-06 19:53:11 +0200

    Read file contents

--- files.rb
+++ files.rb
@@ -8,6 +8,7 @@ txt = File.open(filename)
 puts "Here's your file: #{filename}"
 #read text contents 
 puts txt.read()
+# 
 puts "I'll also ask you to type it again"
 print prompt
 file_again = STDIN.gets.chomp()
