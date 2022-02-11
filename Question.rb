9258ecf50032173986b1b0257bb4a98925079b08
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-02-11 22:18:56 +0200

    Update question

--- Question.rb
+++ Question.rb
@@ -2,9 +2,9 @@ print "How old are you?"
 age = gets.chomp
 print 'How tall are you' 
 height = gets.chomp()
-print "weight yakamitra sei"
+print "weight yakamira sei"
 weight = gets.chomp()
 
-puts "Confirm you rae #{age}, and unorema #{weight} and wakareba #{height} mudenga"
+puts "Confirm you are #{age}, and unorema #{weight} and wakareba #{height} mudenga"
 
 puts %s [weight]
