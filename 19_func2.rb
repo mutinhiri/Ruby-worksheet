77db2bcf9a34ec2cc6eeed192eee022182fc590b
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-12 11:33:56 +0200

    Add changes to check stock files

--- 19_func2.rb
+++ 19_func2.rb
@@ -1,3 +1,5 @@
 def tuckshop_and_beverage(bread, biscuits, cooking_oil)
   puts "Available is #{bread} loaves"
   puts "You have #{biscuits} packs"
+  puts "left is #{cooking_oil} bottles"
+  puts "tank you call again"
