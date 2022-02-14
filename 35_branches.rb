4a85bebcb27c1ae801b7e9b22b836452d012862a
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-14 11:58:49 +0200

    Check if input is within range

--- 35_branches.rb
+++ 35_branches.rb
@@ -6,3 +6,6 @@ def gold_room()
   puts "This room is full of gold how much do you want"
 
   prompt; next_move = gets.chomp()
+
+  if next_move.include? "0" or next_move.include? "50"
+    how_much = 
