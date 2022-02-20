e214cda05cf8520e807759c1d3182bf8368c7721
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-20 13:34:53 +0200

    Add array properties

--- 41-gothons.rb
+++ 41-gothons.rb
@@ -3,4 +3,6 @@ def prompt
 end
 
 def death()
-  quips = ['Hello and welcome ti new new application processes']
+  quips = ['Hello and welcome ti new new application processes',
+  'Check our vision and mission for our reason of being',
+'We hope to wrk with you soon']
