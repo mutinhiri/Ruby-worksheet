9047ded5f9c20729c51a78ea556c04fa204d3955
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-20 13:39:16 +0200

    Add  more changes

--- 41-gothons.rb
+++ 41-gothons.rb
@@ -5,4 +5,11 @@ end
 def death()
   quips = ['Hello and welcome ti new new application processes',
   'Check our vision and mission for our reason of being',
-'We hope to wrk with you soon']
+'We hope to wrk with you soon',
+'Hope to see yoo soon']
+puts quips[rand(quips.length())]
+Process.exit(1)
+end
+
+puts prompt
+return :death
