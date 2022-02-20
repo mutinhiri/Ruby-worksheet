60c344cfed54463e1f4065393304acf751686f83
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-20 06:18:32 +0200

    Add more stuff

--- 39_array_doing_things.rb
+++ 39_array_doing_things.rb
@@ -2,4 +2,4 @@ ten_things = "Apples Oranges Crows Telephone Lights Snooker"
 puts 'wait there are no 10 things in the array'
 
 stuff = ten_things.split(' ')
-puts stuff
+more_stuff = %w(Day Night Frisbee Corn Banana)
