55af24006008cca3716692acc0c9aad02e82cba9
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-21 06:32:47 +0200

    Add quips array for initailaize

--- 42_class_intro.rb
+++ 42_class_intro.rb
@@ -3,5 +3,7 @@ class Game
   def initialize(start)
     @quips = [
       "You died. You kind such at this ",
-      "Nice job, you died.... jackass"
+      "Nice job, you died.... jackass",
+      "Such a luser.",
+      "I have a small puppy that is better at this"
     ]
