6e9763049348085a471612c95f1bac1c802b50a8
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:14:19 +0200

    Add output

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -4,7 +4,7 @@ name = gets.chomp
 puts "enter amount to invest"
 num = gets.chomp.to_i
 
-# result = "Hello #{name}, you will get #{num *2 } value "
+result = "Hello #{name}, you will get #{num *2 } value after 30 days "
 
 # p result
 p "Your name has #{name.length} caracters in it"
