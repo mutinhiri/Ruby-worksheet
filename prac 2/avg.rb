bcf83bbb7cfd6b66ff1b298bdf49bd57240b6a59
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 12:10:45 +0200

    Add changes

--- prac 2/avg.rb
+++ prac 2/avg.rb
@@ -1,11 +1,1 @@
 puts "this is an average count app"
-
-25.times { puts "-"}
-puts "enter num 1"
-num1 = gets.chomp.to_i
-puts 'Enter num 2'
-num2 = gets.chomp.to_f
-puts 'Now clculating average'
-
-10.times { puts "-"}
-puts "your average score is #{average(num1, num2)}"
