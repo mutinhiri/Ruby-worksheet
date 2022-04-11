6b187b022dbc1b1c814205feea7baf0c930e65ab
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:15:06 +0200

    Add app testing

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -5,8 +5,7 @@ puts "enter amount to invest"
 num = gets.chomp.to_i
 
 result = "Hello #{name}, you will get #{num *2 } value after 30 days "
-
 p result
+
 p "Your name has #{name.length} caracters in it"
 puts "your name reversed is #{name.reverse}"
-
