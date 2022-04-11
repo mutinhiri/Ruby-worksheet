d7a8d0a9c984dfdacde779de3ad9451d4de7e2d1
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:34:46 +0200

    Test addition app

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -1,11 +1,1 @@
-puts "what is your name"
-name = gets.chomp
-
-puts "enter amount to invest"
-num = gets.chomp.to_i
-
-result = "Hello #{name}, you will get #{num *2 } value after 30 days "
-p result
-
-p "Your name has #{name.length} caracters in it"
-puts "your name reversed is #{name.reverse} which can be used as password"
+puts "welcome to testing app"
