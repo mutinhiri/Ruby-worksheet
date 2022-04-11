97102e82ed9c84e35a4ca566933f34cee6819183
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:13:56 +0200

    Add amount to use

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -1,8 +1,8 @@
 puts "what is your name"
 name = gets.chomp
 
-# puts "enter amount to invest"
-# num = gets.chomp.to_i
+puts "enter amount to invest"
+num = gets.chomp.to_i
 
 # result = "Hello #{name}, you will get #{num *2 } value "
 
