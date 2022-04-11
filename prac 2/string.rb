ed2e190bbe8bb4ea7dec7085898948b278b1c794
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:09:47 +0200

    Test phase

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -1,11 +1,11 @@
 puts "what is your name"
 
 name = gets.chomp
-puts "enter number tultipky"
-num = gets.chomp.to_i
+# puts "enter number tultipky"
+# num = gets.chomp.to_i
 
 result = "Hello #{name}, you will get #{num *2 } value "
 
 p result
-p "Your name has #{name.count} caracters in it"
+p "Your name has #{name.count()} caracters in it"
 
