ba689ec6d9b8d774c0025aca107e874760bb8618
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-11 09:10:53 +0200

    First character pass

--- prac 2/string.rb
+++ prac 2/string.rb
@@ -4,8 +4,8 @@ name = gets.chomp
 # puts "enter number tultipky"
 # num = gets.chomp.to_i
 
-result = "Hello #{name}, you will get #{num *2 } value "
+# result = "Hello #{name}, you will get #{num *2 } value "
 
-p result
-p "Your name has #{name.count()} caracters in it"
+# p result
+p "Your name has #{name.length} caracters in it"
 
