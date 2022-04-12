e76ca35ca0a5146118c512eeeb1db743bd387ec9
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 08:38:06 +0200

    Add changes

--- prac 2/avg.rb
+++ prac 2/avg.rb
@@ -1,1 +1,12 @@
 # Write a function that receives a string, containing one or more words, and returns a string that contains the same words, but each one is spelled backwards
+
+# def letters(words)
+#     arr = words.split()
+#     for i in arr
+#          output = i.reverse()
+#          puts output.join("")
+#     end
+#     puts output
+# end
+
+# letters('Hello bunbee')
