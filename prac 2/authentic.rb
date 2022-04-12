fa997e145606044d1d3f50c5a0b170cce9a41140
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 09:59:08 +0200

    Fix bug in password

--- prac 2/authentic.rb
+++ prac 2/authentic.rb
@@ -9,4 +9,4 @@ puts "welcome to user app"
 25.times { print "_-"}
 puts 
 puts "This program will take input from the user and compare the password"
-puts "If [assword is correct user object is returned "
+puts "If password is correct user object is returned "
