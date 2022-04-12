3f5142d18ce75a0df82d9ca64364ca8665cc8790
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 08:33:37 +0200

    Add Coding challenge interview

--- prac 2/avg.rb
+++ prac 2/avg.rb
@@ -1,3 +1,10 @@
-puts "Hashes"
+# Write a function that receives a string, containing one or more words, and returns a string that contains the same words, but each one is spelled backwards
 
+def letters(words)
+    arr = words.split()
+    for i in arr
+         output = i.reverse()
+    end
+end
 
+letters('Hello bunbee')
