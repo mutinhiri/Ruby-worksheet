df68916571a6d70ea4835044f0e4dd98f47e3346
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 09:55:50 +0200

    Add users to app

--- prac 2/authentic.rb
+++ prac 2/authentic.rb
@@ -1,3 +1,6 @@
 users = [
-    
+    { username: 'user1', password: 'password1'},
+    { username: 'user2', password: 'password2' },
+    { username: 'user3', password: 'password3' },
+    { username: 'user4', password: 'pssword4' }
 ]
