e632ff00d2bd26b6a00136d7f6a19b0fbf376ce7
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 09:58:47 +0200

    If user is correct

--- prac 2/authentic.rb
+++ prac 2/authentic.rb
@@ -9,3 +9,4 @@ puts "welcome to user app"
 25.times { print "_-"}
 puts 
 puts "This program will take input from the user and compare the password"
+puts "If [assword is correct user object is returned "
