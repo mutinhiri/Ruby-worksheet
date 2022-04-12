42b1baecb6597883028ee306bdc6aab453c65fb4
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-12 12:04:49 +0200

    Add authenticator changes

--- prac 2/authentic.rb
+++ prac 2/authentic.rb
@@ -11,13 +11,13 @@ puts
 puts "This program will take input from the user and compare the password"
 puts "If password is correct user object is returned "
 
-while i < 3
-    puts "Enter username"
-    uname = gets.chomp
-    if uname == 'user1'
-        puts 'user 1 selected'
-    else
-        puts "check user name"
-    end
-    i ++
-end
+# while i < 3
+#     puts "Enter username"
+#     uname = gets.chomp
+#     if uname == 'user1'
+#         puts 'user 1 selected'
+#     else
+#         puts "check user name"
+#     end
+#     i ++
+# end
